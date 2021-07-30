002af1bb384d492e9bc40e57847684effc61b7bc
Yadi Tian <2546988t@student.gla.ac.uk>
2021-07-30 12:04:08 +0100

    complete chapter 9.7

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -101,7 +102,6 @@ def add_page(request, category_name_slug):
                 page.save()
                 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
-        
         else:
             print(form.errors)
     
@@ -146,12 +146,10 @@ def register(request):
             
             # Update our variable to indicate that the template registration was successful.
             registered = True
-        
         else:
             # Invalid form or forms - mistakes or something else?
             # Print problems to the terminal.
             print(user_form.errors, profile_form.errors)
-    
     else:
         # Not a HTTP POST, so we render our form using two ModelForm instances.
         # These forms will be blank, ready for user input.
@@ -160,3 +158,37 @@ def register(request):
     
     # Render the template depending on the context.
     return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+def user_login(request):
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Use Django's machinery to attempt to see if the username/password combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+        
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+            
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the blank dictionary object...
+        return render(request, 'rango/login.html')
